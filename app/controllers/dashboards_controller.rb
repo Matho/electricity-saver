6c9539acc048c0d3b168ca9c2e7d391418c61849
Matho <martin.markech@matho.sk>
2022-12-24 21:26:31 +0100

    Smart plug devices on dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,5 +4,6 @@ class DashboardsController < ApplicationController
   def index
     @uptime_stats = @current_network.uptime_stats.includes(:endpoint_device).last_sorted
     @saved_energies = @current_network.saved_energies.includes(:smart_plug_device).last_sorted
+    @smart_plug_devices = @current_network.smart_plug_devices
   end
 end

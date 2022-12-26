3614c46bb95fb17459f30bac11d2d7d3b68d7e45
Matho <martin.markech@matho.sk>
2022-12-26 12:23:37 +0100

    Add hook to insert event logs

--- app/models/uptime_stat.rb
+++ app/models/uptime_stat.rb
@@ -7,4 +7,19 @@ class UptimeStat < ApplicationRecord
   validates :network_id, presence: true
 
   scope :last_sorted, -> { limit(5).order('id desc') }
+
+  after_commit :create_event_log, on: [:create, :update]
+
+  private
+
+  def create_event_log
+    return nil unless available_previously_changed?
+
+    EventLog.create!({
+                       event_date: Time.current,
+                       endpoint_device_id: self.endpoint_device_id,
+                       network_id: self.network_id,
+                       status: self.available ? :turned_on : :turned_off
+                     })
+  end
 end

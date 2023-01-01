a8dfda7ea30323e5879d4a59e8e2f5da5348115f
Matho <martin.markech@matho.sk>
2023-01-01 18:57:23 +0100

    Try to specify timezone for rspec tests on Github CI

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -76,4 +76,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  #
+  config.before(:suite) do
+    Time.zone = 'Europe/Bratislava'
+  end
 end
